7cae4d637a227e4f10b9616e28de6c41e74ba28b
Fisher <fisher.subs@gmail.com>
2017-10-10 08:18:47 +0800

    [src/resources/Styles] Added App Styles.

---
+++ src/resources/Styles.js
@@ -0,0 +1,15 @@
+/**
+ * Created by fisher at 12:19 on 9/16/17.
+ */
+
+'use strict';
+
+module.exports = {
+	// padding: 20,
+	main: {
+		margin: '0 auto',
+		// maxWidth: 743
+		maxWidth: 1043
+	}
+};
+
