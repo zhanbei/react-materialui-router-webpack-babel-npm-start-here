12f47756a9db125b15d3261c5a3d0da02bf28879
Fisher <fisher.subs@gmail.com>
2017-12-11 19:26:00 +0800

    [package.json] Added npm-install-newest-packages script; updated node modules.

---
+++ npm-install-newest-packages.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Install and save the newest packages required.
+#
+
+npm install --save-dev \
+	babel-cli \
+	babel-core \
+	babel-loader \
+	babel-plugin-transform-class-properties \
+	babel-plugin-transform-object-rest-spread \
+	babel-preset-env babel-preset-react \
+	material-ui@next \
+	material-ui-icons \
+	prop-types \
+	react \
+	react-dom \
+	react-router \
+	react-router-dom \
+	react-tap-event-plugin \
+	webpack
