ab9b094028cfcdd29381139e3bc6fe2f75f672bf
Fisher <fisher.subs@gmail.com>
2018-08-05 20:58:21 +0800

    [src/RouterTransitionExample/App] Implemented transition with react router.
    - following instruction of @see
    https://medium.com/@pshrmn/a-shallow-dive-into-react-router-v4-animated-transitions-4b73f634992a

--- src/RouterTransitionExample/App.js
+++ src/RouterTransitionExample/App.js
@@ -1,17 +1,38 @@
 'use strict';
 
 import React from 'react'
-import {Route, Router, Switch} from 'react-router-dom'
-
+import {Route, Router, Switch, withRouter} from 'react-router-dom'
+import {Transition, CSSTransition, TransitionGroup} from 'react-transition-group';
+import './resources/transition-styles.less';
 import HomePage from './HomePage/HomePage';
 import AboutPage from './AboutPage/AboutPage';
 import TopicsPage from './TopicsPage/TopicsPage';
 import TopicPage from './TopicPage/TopicPage';
 
-import routes from '../resources/AppRoutes';
-
+const routes = require('../resources/AppRoutes');
 const AppHistory = require('../resources/AppHistory');
 const mAppBrowserHistory = AppHistory.getBrowserHistory();
+
+// @see https://reactcommunity.org/react-transition-group/
+// @see https://reacttraining.com/react-router/web/example/animated-transitions
+// @see https://medium.com/@pshrmn/a-shallow-dive-into-react-router-v4-animated-transitions-4b73f634992a
+const RoutesWithTransition = withRouter(({location}) => (
+	<TransitionGroup>
+		<CSSTransition
+			key={location.key}
+			// Keep classNames and timeout consistent with './resources/the transition-styles.less'
+			classNames={'slide-in'}
+			timeout={500}
+		>
+			<Switch location={location}>
+				<Route exact path={routes.ROUTE_HOME} component={HomePage}/>
+				<Route exact path={routes.ROUTE_ABOUT} component={AboutPage}/>
+				<Route exact path={routes.ROUTE_TOPICS} component={TopicsPage}/>
+				<Route exact path={routes.ROUTE_TOPIC} component={TopicPage}/>
+			</Switch>
+		</CSSTransition>
+	</TransitionGroup>
+));
 
 // App entrance used browser router with react-router.
 // @see https://reacttraining.com/react-router/web/example/basic
@@ -19,12 +40,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<Router history={mAppBrowserHistory}>
-				<Switch>
-					<Route exact path={routes.ROUTE_HOME} component={HomePage}/>
-					<Route exact path={routes.ROUTE_ABOUT} component={AboutPage}/>
-					<Route exact path={routes.ROUTE_TOPICS} component={TopicsPage}/>
-					<Route exact path={routes.ROUTE_TOPIC} component={TopicPage}/>
-				</Switch>
+				<RoutesWithTransition/>
 			</Router>
 		)
 	}
