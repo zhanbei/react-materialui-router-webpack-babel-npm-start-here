b3a2ab5b79fd99c2ea3b626041ebb54a59d05a98
Fisher <fisher.subs@gmail.com>
2018-08-05 13:07:14 +0800

    [src/*] Updated references to @material-ui/core instead of material-ui.

--- src/RouterExample/HomePage/HomePage.js
+++ src/RouterExample/HomePage/HomePage.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import React from 'react';
-import {withStyles} from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
+import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const mAppBrowserHistory = require('../../resources/AppHistory').getBrowserHistory();
 
--- src/RouterExample/TopicsPage/TopicsPage.js
+++ src/RouterExample/TopicsPage/TopicsPage.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import React from 'react';
-import {withStyles} from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from "material-ui/IconButton";
+import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const mAppBrowserHistory = require('../../resources/AppHistory').getBrowserHistory();
