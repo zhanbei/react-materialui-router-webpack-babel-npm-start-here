c939207601432d8edb9a74ce95ae9838b72527c4
Fisher <fisher.subs@gmail.com>
2018-08-14 18:34:17 +0800

    [src/resources/MuiStyles] Upgraded styles of a page with/without side navigator.
    - Following rules bellow:
    - Page Holder `(absolute{top: 0; display: flex; flex-flow: column; width: 100%; height: 100%;})` `[selected]`
    - App Bar `(static)`: take the top part of the page.
    - Drawer Holder `{width: '35%', minWidth: '280px', maxWidth: '360px'}`
    - Body Holder `{flex: 1, display: 'flex'}`: will follow the app bar and take the bottom of the page.
    - Left Navigation Holder `{width: '35%', minWidth: '280px', maxWidth: '360px', borderRight: '1px solid #0000001f'}`: take the left part with a particular width.
    - Content Holder `{flex: 1; overflow: auto;}`: take the right part left.
    - Main Content Holder `{margin: '0 auto', maxWidth: '960px'}`: center the main content.
    - Main Content Paper Holder `{padding: 2*unit, margin: (2*unit)+'px 0'}`
    - Main Content Padding Holder `{padding: 2*unit}`

--- src/resources/MuiStyles.js
+++ src/resources/MuiStyles.js
@@ -3,8 +3,29 @@
 module.exports = theme => {
 	const unit = theme.spacing.unit;
 	return {
+		// The style for each standalone single page, which usually has a app bar,
+		// a optional left navigation, and a body with main content.
+		pageHolder: {display: 'flex', flexFlow: 'column nowrap', position: 'absolute', width: '100%', height: '100%'},
+		// The style for the app bar, which displays above the left navigation.
+		appBar: {zIndex: theme.zIndex.drawer + 1},
+		// The style for the app bar, which often has a min height of 64px.
 		toolbar: theme.mixins.toolbar,
+		// The style for the left side temporary drawer.
+		drawerHolder: {width: '35%', minWidth: '280px', maxWidth: '360px'},
+		// The style for the main body which holds the main content.
+		bodyHolder: {flex: 1, display: 'flex'},
+		// The style for the optional left navigation which, in small-screen devices, should be
+		//  hidden and an extra drawer will be used for alternative.
+		leftNavigationHolder: {width: '35%', minWidth: '280px', maxWidth: '360px', borderRight: '1px solid #0000001f'},
+		contentHolder: {flex: 1, overflow: 'auto'},
+		// Used for the main content of a page, which limits its max-width and keeps it center.
+		// @see https://material.io/guidelines/layout/responsive-ui.html#responsive-ui-breakpoints
 		mainContentHolder: {margin: '0 auto', maxWidth: '960px'},
+		// @obsolete IMPORTANT THE STYLE IS OBSOLETE NOW, AND MAY BE REMOVED ANYTIME FROM NOW ON.
 		mainContentWithPaddingHolder: {margin: '0 auto', maxWidth: '960px', padding: 2 * unit},
+		// A style with padding around for the #mainContent.
+		mainContentPaddingHolder: {padding: 2 * unit},
+		// A style with 2-unit padding around and 2-unit vertical margin for the #mainContent, which is usually for a #Paper.
+		mainContentPaperHolder: {padding: unit * 2, margin: (unit * 2) + 'px 0'},
 	};
 };
